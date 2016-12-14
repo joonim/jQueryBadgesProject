3b95c474cf6109278e4e3bb195f6b260b4ae6d89
Joon Im <joonim12@gmail.com>
2016-12-14 09:37:27 -0500

    add and show list of badges

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,14 @@
 $(function() {
-
   // your code will go here
-
+  $.ajax({
+    url: 'https://www.codeschool.com/users/sergiocruz.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      // handle response
+      $.each(response.courses.completed, function(i, course){
+        var div = $('<div class="course"><h3>'+course.title+'</h3><img src="'+course.badge+'"><a href="'+course.url+'" target="_blank" class="btn btn-primary">See Course</a></div>');
+        $('#badges').append(div);
+      });
+    }
+  });
 });
